a7cbc04c63ea52c50530b881f7db65a37b239c8b
Zhiqiang Zhao <learningambassador@163.com>
2025-12-29 16:31:52 +0800

    update use case

--- docs/conf.py
+++ docs/conf.py
@@ -1,97 +1,226 @@
+"""
+Shared Sphinx configuration using sphinx-multiproject.
+
+To build each project, the ``PROJECT`` environment variable is used.
+
+.. code:: console
+
+   $ make html  # build default project
+   $ PROJECT=dev make html  # build the dev project
+
+for more information read https://sphinx-multiproject.readthedocs.io/.
+"""
+
 import os
 import sys
-from datetime import datetime
 
-# -- 多项目配置 -------------------------------------------------------
+from multiproject.utils import get_project
+
+sys.path.append(os.path.abspath("_ext"))
+
 extensions = [
+    "multiproject",
     "myst_parser",
-    # "autodoc2",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx_design",
+    # For testing, conditionally disable the custom 404 pages on dev docs
+    # "notfound.extension",
     "sphinx_copybutton",
-    # "sphinxext.rediraffe",
-    # disabled due to https://github.com/mgaitan/sphinxcontrib-mermaid/issues/109
-    "sphinxcontrib.mermaid",
+    "sphinx_design",
+    "sphinx_tabs.tabs",
+    "sphinx_prompt",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinxcontrib.httpdomain",
+    "sphinxcontrib.video",
+    "sphinxemoji.sphinxemoji",
     "sphinxext.opengraph",
-    "sphinx_pyscript",
-    "sphinx_tippy",
-    "sphinx_togglebutton",
 ]
 
-# 1. 定义多项目映射
 multiproject_projects = {
-    "product_a": {
-        "path": "product_a",
-        "title": "Product A Documentation",
-        "version": "1.0",
+    "user": {
+        "use_config_file": False,
+        "config": {
+            "project": "Read the Docs user documentation",
+            "html_title": "Read the Docs user documentation",
+        },
     },
-    "product_b": {
-        "path": "product_b", 
-        "title": "Product B Guide",
-        "version": "2.1",
+    "dev": {
+        "use_config_file": False,
+        "config": {
+            "project": "Read the Docs developer documentation",
+            "html_title": "Read the Docs developer documentation",
+        },
     },
 }
 
-# 2. 从环境变量获取当前构建的项目 (由 .readthedocs.yaml 传递)
-current_project = os.environ.get('multiproject', 'product_a')
+docset = get_project(multiproject_projects)
 
-# -- 根据当前项目进行条件配置 -----------------------------------------
-if current_project == 'product_a':
-    # 产品A专属配置
-    project = 'Product A'
-    html_title = f"{project} Docs v1.0"
-    # 源文件路径
-    master_doc = 'index'
-    exclude_patterns = ['product_b/**', 'shared/_templates/**']
-    
-elif current_project == 'product_b':
-    # 产品B专属配置  
-    project = 'Product B'
-    html_title = f"{project} User Guide v2.1"
-    master_doc = 'index'
-    exclude_patterns = ['product_a/**', 'shared/_templates/**']
+# Disable custom 404 on dev docs
+if docset == "user":
+    extensions.append("notfound.extension")
 
-# -- 通用配置 (所有项目共享) -----------------------------------------
-author = 'Your Team'
-copyright = f'{datetime.now().year}, {author}'
+ogp_site_name = "Read the Docs Documentation"
+ogp_use_first_image = True  # https://github.com/readthedocs/blog/pull/118
+ogp_image = "https://docs.readthedocs.io/en/latest/_static/img/logo-opengraph.png"
+# Inspired by https://github.com/executablebooks/MyST-Parser/pull/404/
+ogp_custom_meta_tags = (
+    '<meta name="twitter:card" content="summary_large_image" />',
+)
+ogp_enable_meta_description = True
+ogp_description_length = 300
 
-# MyST 配置
+templates_path = ["_templates"]
+
+# This may be elevated as a general issue for documentation and behavioral
+# change to the Sphinx build:
+# This will ensure that we use the correctly set environment for canonical URLs
+# Old Read the Docs injections makes it point only to the default version,
+# for instance /en/stable/
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+
+master_doc = "index"
+copyright = "Read the Docs, Inc & contributors"
+version = "15.10.0"
+release = version
+exclude_patterns = ["_build", "shared", "_includes"]
+# Exclude design docs from dev documentation
+if docset == "dev":
+    exclude_patterns.append("design")
+default_role = "obj"
+intersphinx_cache_limit = 14  # cache for 2 weeks
+intersphinx_timeout = 3  # 3 seconds timeout
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3.10/", None),
+    "django": (
+        "https://docs.djangoproject.com/en/stable/",
+        "https://docs.djangoproject.com/en/stable/_objects/",
+    ),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "pip": ("https://pip.pypa.io/en/stable/", None),
+    "nbsphinx": ("https://nbsphinx.readthedocs.io/en/latest/", None),
+    "myst-nb": ("https://myst-nb.readthedocs.io/en/stable/", None),
+    "ipywidgets": ("https://ipywidgets.readthedocs.io/en/stable/", None),
+    "jupytext": ("https://jupytext.readthedocs.io/en/stable/", None),
+    "ipyleaflet": ("https://ipyleaflet.readthedocs.io/en/latest/", None),
+    "poliastro": ("https://docs.poliastro.space/en/stable/", None),
+    "myst-parser": ("https://myst-parser.readthedocs.io/en/stable/", None),
+    "writethedocs": ("https://www.writethedocs.org/", None),
+    "jupyterbook": ("https://jupyterbook.org/en/stable/", None),
+    "executablebook": ("https://executablebooks.org/en/latest/", None),
+    "rst-to-myst": ("https://rst-to-myst.readthedocs.io/en/stable/", None),
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "rtd-dev": ("https://dev.readthedocs.io/en/latest/", None),
+    "rtd-blog": ("https://blog.readthedocs.com/", None),
+    "jupyter": ("https://docs.jupyter.org/en/latest/", None),
+}
+
+# Intersphinx: Do not try to resolve unresolved labels that aren't explicitly prefixed.
+# The default setting for intersphinx_disabled_reftypes can cause some pretty bad
+# breakage because we have rtd and rtd-dev stable versions in our mappings.
+# Hence, if we refactor labels, we won't see broken references, since the
+# currently active stable mapping keeps resolving.
+# Recommending doing this on all projects with Intersphinx.
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
+intersphinx_disabled_reftypes = ["*"]
+
 myst_enable_extensions = [
-    "dollarmath",
-    "amsmath",
     "deflist",
-    "fieldlist",
-    "html_admonition",
-    "html_image",
-    "colon_fence",
-    "smartquotes",
-    "replacements",
-    # "linkify",
-    "strikethrough",
-    "substitution",
-    "tasklist",
-    "attrs_inline",
-    "attrs_block",
+]
+htmlhelp_basename = "ReadTheDocsdoc"
+latex_documents = [
+    (
+        "index",
+        "ReadTheDocs.tex",
+        "Read the Docs Documentation",
+        "Eric Holscher, Charlie Leifer, Bobby Grace",
+        "manual",
+    ),
+]
+man_pages = [
+    (
+        "index",
+        "read-the-docs",
+        "Read the Docs Documentation",
+        ["Eric Holscher, Charlie Leifer, Bobby Grace"],
+        1,
+    )
 ]
 
-# 主题配置
-html_theme = 'sphinx_rtd_theme'
+language = "en"
+
+locale_dirs = [
+    f"{docset}/locale/",
+]
+gettext_compact = False
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static", f"{docset}/_static"]
+html_css_files = ["css/custom.css", "css/sphinx_prompt_css.css"]
+html_js_files = ["js/expand_tabs.js"]
+
+html_logo = "img/logo.svg"
 html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
-    'style_external_links': True,
+    "logo_only": True,
+}
+html_context = {
+    # Fix the "edit on" links.
+    # TODO: remove once we support different rtd config
+    # files per project.
+    "conf_py_path": f"/docs/{docset}/",
+    "display_github": True,
+    "github_user": "readthedocs",
+    "github_repo": "readthedocs.org",
+    "github_version": "main",
+    # Use to generate the Plausible "data-domain" attribute from the template
+    "plausible_domain": f"{os.environ.get('READTHEDOCS_PROJECT')}.readthedocs.io",
 }
 
-# 路径设置
-templates_path = ['shared/_templates']
-html_static_path = ['shared/_static', f'{current_project}/_static']
-html_css_files = ['custom.css']
+# See dev/style_guide.rst for documentation
+rst_epilog = """
+.. |org_brand| replace:: Read the Docs Community
+.. |com_brand| replace:: Read the Docs for Business
+.. |git_providers_and| replace:: GitHub, Bitbucket, and GitLab
+.. |git_providers_or| replace:: GitHub, Bitbucket, or GitLab
+"""
 
-# 源文件后缀
-source_suffix = {
-    '.md': 'markdown',
-    '.rst': 'restructuredtext',
+# Activate autosectionlabel plugin
+autosectionlabel_prefix_document = True
+
+# sphinx-notfound-page
+# https://github.com/readthedocs/sphinx-notfound-page
+notfound_context = {
+    "title": "Page Not Found",
+    "body": """
+<h1>Page Not Found</h1>
+
+<p>Sorry, we couldn't find that page.</p>
+
+<p>Try using the search box or go to the homepage.</p>
+""",
 }
+linkcheck_retries = 2
+linkcheck_timeout = 1
+linkcheck_workers = 10
+linkcheck_ignore = [
+    r"http://127\.0\.0\.1",
+    r"http://localhost",
+    r"http://community\.dev\.readthedocs\.io",
+    r"https://yourproject\.readthedocs\.io",
+    r"https?://docs\.example\.com",
+    r"https://foo\.readthedocs\.io/projects",
+    r"https://github\.com.+?#L\d+",
+    r"https://github\.com/readthedocs/readthedocs\.org/issues",
+    r"https://github\.com/readthedocs/readthedocs\.org/pull",
+    r"https://docs\.readthedocs\.io/\?rtd_search",
+    r"https://readthedocs\.org/search",
+    # This page is under login
+    r"https://readthedocs\.org/accounts/gold",
+]
+
+extlinks = {
+    "rtd-issue": ("https://github.com/readthedocs/readthedocs.org/issues/%s", "#%s"),
+}
+
+# Disable epub mimetype warnings
+suppress_warnings = ["epub.unknown_project_files"]
--- docs/prodcut_a/KernelGen_overview/requirements.md
+++
@@ -1,6 +0,0 @@
-# Requirements
-
-You are recommended to use the following web browsers:
-
-- Chrome major version greater than 100
-- Microsoft Edge major version greater than 100
